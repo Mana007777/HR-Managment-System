9a3450fe5713fa06e3f6beb2152c107e5291f541
mana007777 <e7ifubbb@gmail.com>
2026-03-22 09:08:05 +0300

    Companies Seeders

--- database/seeders/CompaniesSeeder.php
+++ database/seeders/CompaniesSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CompaniesSeeder extends Seeder
 {
@@ -12,6 +14,39 @@ class CompaniesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('companies')->insert([
+            [
+                'name' => 'Optimal Solutions Ltd.',
+                'email' => 'info@optimalsolutions.com',
+                'website' => 'https://www.optimalsolutions.com',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Tech Solutions Inc.',
+                'email' => 'info@techsolutions.com',
+                'website' => 'https://www.techsolutions.com',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Gateway Technologies',
+                'email' => 'info@gatewaytech.com',
+                'website' => 'https://www.gatewaytech.com',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Innovatech Systems',
+                'email' => 'info@innovatech.com',
+                'website' => 'https://www.innovatech.com',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            ]);
+
+            foreach(Company::all() as $key => $company) {
+                $company->users()->attach(1);
+            }
     }
 }
