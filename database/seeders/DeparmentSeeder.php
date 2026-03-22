9db55b29262cbe15276ae4a96eaf4a9fc4a0cf12
mana007777 <e7ifubbb@gmail.com>
2026-03-22 09:16:16 +0300

    Department Seeders

--- database/seeders/DeparmentSeeder.php
+++ database/seeders/DeparmentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,59 @@ class DeparmentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $companies = Company::all();
+
+        foreach ($companies as $company) {
+            $departments = $company->departments()->createMany([
+                ['name' => 'Human Resources'],
+                ['name' => 'Finance'],
+                ['name' => 'Engineering'],
+                ['name' => 'Marketing'],
+            ]);
+        }
+
+        foreach ($departments as $department) {
+                switch ($department->name) {
+                    case 'Human Resources':
+                        $designations = 
+                        [
+                            'HR Manager',
+                            'Recruiter',
+                            'HR Coordinator',
+                        ];
+                        break;
+                        case 'Finance':
+                        $designations = 
+                        [
+                            'Finance Manager',
+                            'Accountant',
+                            'Financial Analyst',
+                        ];
+                        break;
+                        case 'Engineering':
+                        $designations = 
+                        [
+                            'Engineering Manager',
+                            'Software Engineer',
+                            'DevOps Engineer',
+                        ];
+                        break;
+                        case 'Marketing':
+                        $designations = 
+                        [
+                            'Marketing Manager',
+                            'Marketing Specialist',
+                            'Content Creator',
+                        ];
+                        break;
+                    
+                    default:
+                            $designations = [];
+                    break;
+                }
+                foreach ($designations as $designation) {
+                    $department->designations()->create(['name' => $designation]);
+                }
+            }
     }
 }
