6efeb2d40c29a71bea3e94f876ee4626295350bd
mana007777 <e7ifubbb@gmail.com>
2026-03-23 11:43:33 +0300

    employee edit

--- resources/views/components/admin/employees/⚡edit.blade.php
+++ resources/views/components/admin/employees/⚡edit.blade.php
@@ -1,10 +1,52 @@
 <?php
 
+use App\Models\Designation;
+use App\Models\Employee;
 use Livewire\Component;
 
 new class extends Component
 {
-    //
+     public $employee;
+    public $department_id;
+
+    public function rules()
+    {
+        return [
+            'employee.name' => 'required|string|max:255',
+            'employee.email' => 'required|email|unique:employees,email',
+            'employee.phone' => 'required|string|max:255',
+            'employee.address' => 'required|string|max:255',
+            'employee.designation_id' => 'required|exists:designations,id',
+        ];
+    }
+
+    public function mount($id)
+    {
+        $this->employee = Employee::find($this->employeeId);
+        $this->department_id = $this->employee->designation->department_id;
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $this->employee->save();
+
+        session()->flash('message', 'Employee updated successfully.');
+
+        return $this->redirect(route('employees.index'));
+    }
+
+    public function getDesignationsProperty()
+    {
+        if (!$this->department_id) {
+            return collect();
+        }
+
+        return Designation::inCompany()
+            ->where('department_id', $this->department_id)
+            ->get();
+    }
 };
 ?>
 
